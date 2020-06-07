82366a53f37670b8cc926765fea6c9c445b19b84
Joshua <josh.plateros13@gmail.com>
2020-06-07 15:39:28 -0700

    Fixed FA.py

---
+++ FA.py
@@ -0,0 +1,32 @@
+import os
+import json
+import time
+
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+class MyHandler(FileSystemEventHandler):
+    def on_modified(self,event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track + "/" + filename
+            if (filename.endswith('.txt')):
+                folder_destination = "/mnt/d/Josh/Documents/textFiles/"
+            elif (filename.endswith('.png') or filename.endswith('.jpg')):
+                folder_destination = "/mnt/d/Josh/Documents/pictureFiles/"
+            elif (filename.endswith('.zip')):
+                folder_destination = "/mnt/d/Josh/Documents/zipFiles/"
+            new_destination = folder_destination + "/" + filename
+            os.rename(src, new_destination)
+
+folder_to_track = "/mnt/d/Josh/Documents/organize/"
+folder_destination = "/mnt/d/Josh/Documents/testFolder/"
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
